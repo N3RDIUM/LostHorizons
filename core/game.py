dd32e7bba999a2e4e8b0e76261d67846c9d6d08c
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-13 14:16:03 +0530

    Using multiprocessing to speed things up

--- core/game.py
+++ core/game.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 class Game(object):
     def __init__(self, window):
         """
@@ -7,6 +9,15 @@ class Game(object):
         self.window = window
         self.window.schedule_mainloop(self)
         self.window.schedule_shared_context(self)
+        
+        self.processes = []
+        self.manager = multiprocessing.Manager()
+        self.namespace = self.manager.Namespace()
+        self.namespace.queue = self.manager.Queue()
+        self.namespace.queue.put("Hello world!")
+        for i in range(multiprocessing.cpu_count() + 1):
+            self.processes.append(multiprocessing.Process(target=self.process, args=(self.namespace,)))
+            self.processes[i].start()
         
     def drawcall(self):
         """
@@ -19,3 +30,13 @@ class Game(object):
         Shared context.
         """
         pass
+    
+    @staticmethod
+    def process(namespace):
+        """
+        This function is called to start a multiprocessing process.
+        """
+        # Get all items from the queue
+        queue = namespace.queue
+        while not queue.empty():
+            print(queue.get())
