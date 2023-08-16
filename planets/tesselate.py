80eef81afe7717b40c003be7ca55f2b89827b1b9
n3rdium <leenagajbhiye1003@gmail.com>
2023-08-16 17:01:10 +0530

    We have a triangle mesh!

--- planets/tesselate.py
+++ planets/tesselate.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 @lru_cache(maxsize=128)
 def tesselate(quad, segments=100):
     """
-    This function takes a 3d quad and returns a grid of points
+    This function takes a 3d quad and returns a triangle mesh
     in that quad such that the grid is segments x segments in size.
     This comes in handy when we want to do terrain stuff.
     """
@@ -15,12 +15,28 @@ def tesselate(quad, segments=100):
     for y in range(segments + 1):
         for x in range(segments + 1):
             # Calculate the point and add it to the list
-            point = (
+            point1 = (
                 p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * y / segments,
                 p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * y / segments,
                 p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * y / segments
             )
-            new_verts.append(point)
+            point2 = (
+                p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * (y + 1) / segments,
+                p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * (y + 1) / segments,
+                p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            )
+            point3 = (
+                p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * y / segments,
+                p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * y / segments,
+                p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * y / segments
+            )
+            point4 = (
+                p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * (y + 1) / segments,
+                p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * (y + 1) / segments,
+                p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            )
+            # Add the points to the list
+            new_verts.extend((point1, point2, point3, point3, point2, point4))
     # Return the new vertices
     return new_verts
 
@@ -39,11 +55,27 @@ def tesselate_partial(quad, segments=100, denominator=4, numerator=1):
     for y in range(yrange_start, yrange_end):
         for x in range(segments + 1):
             # Calculate the point and add it to the list
-            point = (
+            point1 = (
                 p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * y / segments,
                 p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * y / segments,
                 p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * y / segments
             )
-            new_verts.append(point)
+            point2 = (
+                p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * (y + 1) / segments,
+                p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * (y + 1) / segments,
+                p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            )
+            point3 = (
+                p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * y / segments,
+                p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * y / segments,
+                p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * y / segments
+            )
+            point4 = (
+                p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * (y + 1) / segments,
+                p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * (y + 1) / segments,
+                p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            )
+            # Add the points to the list
+            new_verts.extend((point1, point2, point3, point3, point2, point4))
     # Return the new vertices
     return new_verts
