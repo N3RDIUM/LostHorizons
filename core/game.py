8184baabcfe68420014398d7935a352fc319c6b2
n3rdium <n3rdium@gmail.com>
2023-09-27 17:38:11 +0530

    Craters are coming soon!

--- core/game.py
+++ core/game.py
@@ -1,24 +1,13 @@
-import math
 import multiprocessing
 
 from core.renderer import Renderer
-from core.util import normalize
 from camera.player import Player
 from planets.tesselate import tesselate_partial
-from planets.planet import Planet
 
+import random
 import noise
 
-from camera.player import Player
-from core.renderer import Renderer
-from core.util import normalize
-from planets.leafnode import LeafNode
-from planets.planet import DummyPlanet
-from planets.tesselate import tesselate_partial
-
-
 class Game(object):
-
     def __init__(self, window):
         """
         class Game
@@ -27,7 +16,7 @@ class Game(object):
         self.window = window
         self.renderer = Renderer()
         self.player = Player()
-
+        
         self.processes = []
         self.manager = multiprocessing.Manager()
         self.namespace = self.manager.Namespace()
@@ -38,91 +27,68 @@ class Game(object):
         self.process_count = multiprocessing.cpu_count()
         for i in range(self.process_count):
             self.processes.append(
-                multiprocessing.Process(target=self.process,
-                                        args=(self.namespace, )))
+                multiprocessing.Process(target=self.process, args=(self.namespace,)))
             self.processes[i].start()
             
-        self.planet = Planet(
-            renderer=self.renderer,
-            game=self,
-        )
+        for i in range(len(self.processes)):
+            self.addToQueue({
+                "task": "tesselate",
+                "mesh": "default",
+                "quad": [
+                    (-1, -1, -1),
+                    (1, -1, -1),
+                    (1, -1, 1),
+                    (-1, -1, 1)
+                ],
+                "segments": 100,
+                "denominator": len(self.processes),
+                "numerator": i
+            })
         
         self.window.schedule_mainloop(self)
         self.window.schedule_shared_context(self)
-
+            
     def addToQueue(self, item):
         """
         Add an item to the queue.
         """
         self.namespace.queue.put(item)
-
+    
     @staticmethod
     def process(namespace):
         """
         This function is called to start a multiprocessing process.
         """
         import glfw
-
         glfw.init()
-
+        
         # Get all items from the queue
         queue = namespace.queue
         while not namespace.killed:
             if not queue.empty():
                 item = queue.get()
                 Game.handleQueueItem(item, namespace)
-
+                
         if namespace.killed:
             glfw.terminate()
-
+            
     @staticmethod
     def handleQueueItem(item, namespace):
         """
         Handle a queue item.
         """
-        if item["task"] == "generate_leafnode":
-            # Tesselate
+        if item["task"] == "tesselate":
+            # Vertex calculations
             quad = tuple(item["quad"])
-            newquad = []
-            for i in range(len(quad)):
-                newquad.append(tuple(quad[i]))
-            quad = tuple(newquad)
             segments = item["segments"]
-            _new_verts = tesselate_partial(quad, segments, item["denominator"],
-                                           item["numerator"])
+            _new_verts = tesselate_partial(quad, segments, item["denominator"], item["numerator"])
             for i in range(len(_new_verts)):
                 _new_verts[i] = (
                     _new_verts[i][0] * segments,
-                    item["planet_radius"] + item["planet_center"][1],
+                    _new_verts[i][1] + noise.pnoise3(_new_verts[i][0], _new_verts[i][1], _new_verts[i][2]) * 10,
                     _new_verts[i][2] * segments,
                 )
-
-            # Spherify
-            CENTER = item["planet_center"]
-            RADIUS = item["planet_radius"]
-            for i in range(len(_new_verts)):
-                v = _new_verts[i]
-                x = v[0] - CENTER[0]
-                y = v[1] - CENTER[1]
-                z = v[2] - CENTER[2]
-
-                length = math.sqrt(x**2 + y**2 + z**2)
-
-                x = x / length * RADIUS
-                y = y / length * RADIUS
-                z = z / length * RADIUS
-
-                # Add noise
-                vector = [x, y, z]
-                vector = normalize(vector)
-                _noise = 1 # No noise for now
-                
-                x = x + vector[0] * _noise
-                y = y + vector[1] * _noise
-                z = z + vector[2] * _noise
-
-                _new_verts[i] = [x, y, z]
-
+            verts_1d = [item for sublist in _new_verts for item in sublist]
             # Color calculations based on perlin noise in that area
             colors = []
             for i in range(len(_new_verts)):
@@ -130,47 +96,27 @@ class Game(object):
                 y = _new_verts[i][1]
                 z = _new_verts[i][2]
                 colors.extend((
-                    abs(
-                        noise.pnoise3(x / 10 + 8, y / 10 + 8, z / 10 + 8) / 4 *
-                        3 + 0.25),
-                    abs(
-                        noise.pnoise3(x / 10 + 16, y / 10 + 16, z / 10 + 16) /
-                        4 * 3 + 0.25),
-                    abs(
-                        noise.pnoise3(x / 10 + 32, y / 10 + 32, z / 10 + 32) /
-                        4 * 3 + 0.25),
+                    abs(noise.pnoise3(x / 10 + 8, y / 10 + 8, z / 10 + 8) / 4 * 3 + 0.25), 
+                    abs(noise.pnoise3(x / 10 + 16, y / 10 + 16, z / 10 + 16) / 4 * 3 + 0.25),
+                    abs(noise.pnoise3(x / 10 + 32, y / 10 + 32, z / 10 + 32) / 4 * 3 + 0.25)
                 ))
-                
-            # Get the average position (center) of the vertices
-            center = [0, 0, 0]
-            for i in range(len(_new_verts)):
-                center[0] += _new_verts[i][0]
-                center[1] += _new_verts[i][1]
-                center[2] += _new_verts[i][2]
-            center[0] /= len(_new_verts)
-            center[1] /= len(_new_verts)
-            center[2] /= len(_new_verts)
-                
-            verts_1d = [item for sublist in _new_verts for item in sublist]
-            namespace.storages[item["mesh"]].vertices.extend(verts_1d)
-            namespace.storages[item["mesh"]].colors.extend(colors)
-
+            namespace.storages['default'].vertices.extend(verts_1d)
+            namespace.storages['default'].colors.extend(colors)
+            
     def terminate(self):
         """
         Terminate all processes.
         """
         self.namespace.killed = True
         for process in self.processes:
-            process.join()
-        for process in self.processes:
             process.terminate()
-
+                
     def drawcall(self):
         """
         Draw call.
         """
         self.player.update(self.window.window)
-        self.planet.draw()
+        self.renderer.draw()
 
     def sharedcon(self):
         """
--- planets/leafnode.py
+++ planets/leafnode.py
@@ -1,15 +1,15 @@
 from uuid import uuid4
 
-
 class LeafNode(object):
-
-    def __init__(self,
-                 quad,
-                 segments=64,
-                 parent=None,
-                 planet=None,
-                 renderer=None,
-                 game=None):
+    def __init__(
+        self,
+        quad,
+        segments = 64,
+        parent = None,
+        planet = None,
+        renderer = None,
+        game = None
+    ):
         """
         LeafNode
         """
@@ -20,31 +20,19 @@ class LeafNode(object):
         self.renderer = renderer
         self.game = game
         self.uuid = uuid4()
-
+        
     def generate(self):
         """
         Schedule the generation of this chunk using multiprocessing.
         """
         self.mesh = self.renderer.create_storage(self.uuid)
-
+        
         for i in range(len(self.game.processes)):
             self.game.addToQueue({
-                # Task type
-                "task": "generate_leafnode",
-                # Details for mesh
+                "task": "tesselate",
                 "mesh": self.uuid,
-                # Details for tesselation
                 "quad": self.quad,
-                "segments": 64,
+                "segments": self.segments,
                 "denominator": len(self.game.processes),
                 "numerator": i,
-                # Details for spherification
-                "planet_center": self.planet.position,
-                "planet_radius": self.planet.radius,
             })
-            
-    def draw(self):
-        """
-        Draw the chunk.
-        """
-        self.renderer.draw_storage(self.uuid)
