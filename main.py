dc7a571db4c7c39523a80e79041b2bf4c0ef4c89
n3rdium <n3rdium@gmail.com>
2024-03-24 20:57:45 +0530

    FASTER

--- main.py
+++ main.py
@@ -19,27 +19,44 @@ def fast_tesselate():
     p1 = [coords[0, 0], coords[0, 1], coords[0, 2]]
     p2 = [coords[1, 0], coords[1, 1], coords[1, 2]]
     p4 = [coords[3, 0], coords[3, 1], coords[3, 2]]
-
-    ti.loop_config(serialize=True)
+    
+    inv_segments = 1 / segments
+    
+    ti.loop_config(parallelize=True)
     for y in range(segments + 1):
+        yp1 = (y + 1)
+        ti.loop_config(parallelize=True)
         for x in range(segments + 1):
-            new_verts[(y * (segments + 1) + x) * 12 +  0] = p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * y / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  1] = p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * y / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  2] = p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * y / segments
+            current = (y * (segments + 1) + x) * 12
             
-            new_verts[(y * (segments + 1) + x) * 12 +  3] = p1[0] + (p2[0] - p1[0]) * x / segments + (p4[0] - p1[0]) * (y + 1) / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  4] = p1[1] + (p2[1] - p1[1]) * x / segments + (p4[1] - p1[1]) * (y + 1) / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  5] = p1[2] + (p2[2] - p1[2]) * x / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            a = (p2[0] - p1[0]) * x * inv_segments + (p4[0] - p1[0])
+            b = (p2[1] - p1[1]) * x * inv_segments + (p4[1] - p1[1])
+            c = (p2[2] - p1[2]) * x * inv_segments + (p4[2] - p1[2])
             
-            new_verts[(y * (segments + 1) + x) * 12 +  6] = p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * y / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  7] = p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * y / segments
-            new_verts[(y * (segments + 1) + x) * 12 +  8] = p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * y / segments
+            new_verts[current +  0] = p1[0] + a * y * inv_segments
+            new_verts[current +  1] = p1[1] + b * y * inv_segments
+            new_verts[current +  2] = p1[2] + c * y * inv_segments
             
-            new_verts[(y * (segments + 1) + x) * 12 +  9] = p1[0] + (p2[0] - p1[0]) * (x + 1) / segments + (p4[0] - p1[0]) * (y + 1) / segments
-            new_verts[(y * (segments + 1) + x) * 12 + 10] = p1[1] + (p2[1] - p1[1]) * (x + 1) / segments + (p4[1] - p1[1]) * (y + 1) / segments
-            new_verts[(y * (segments + 1) + x) * 12 + 11] = p1[2] + (p2[2] - p1[2]) * (x + 1) / segments + (p4[2] - p1[2]) * (y + 1) / segments
+            new_verts[current +  3] = p1[0] + a * yp1 * inv_segments
+            new_verts[current +  4] = p1[1] + b * yp1 * inv_segments
+            new_verts[current +  5] = p1[2] + x * yp1 * inv_segments
             
-t = perf_counter()  
-fast_tesselate()
-print(new_verts)
-print(f"done in {perf_counter() - t}s")
+            d = (p2[0] - p1[0]) * (x + 1) * inv_segments + (p4[0] - p1[0])
+            e = (p2[1] - p1[1]) * (x + 1) * inv_segments + (p4[1] - p1[1])
+            f = (p2[2] - p1[2]) * (x + 1) * inv_segments + (p4[2] - p1[2])
+            
+            new_verts[current +  6] = p1[0] + d * y * inv_segments
+            new_verts[current +  7] = p1[1] + e * y * inv_segments
+            new_verts[current +  8] = p1[2] + f * y * inv_segments
+            
+            new_verts[current +  9] = p1[0] + d * yp1 * inv_segments
+            new_verts[current + 10] = p1[1] + e * yp1 * inv_segments
+            new_verts[current + 11] = p1[2] + f * yp1 * inv_segments
+
+t = perf_counter()
+n = 100
+for i in range(n):
+    fast_tesselate()
+    print(new_verts)
+new = perf_counter()
+print(f"Done in {new - t}s! That's {(new - t) / n}s per operation!")
